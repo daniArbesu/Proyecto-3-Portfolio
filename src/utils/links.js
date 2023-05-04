783cb4a89ab1eb5a7db66064d9b18374e554de7c
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-04 16:43:33 +0200

    feat: add links hero

---
+++ src/utils/links.js
@@ -0,0 +1,21 @@
+export const ctaLinks = [
+  {
+    name: 'LinkedIN',
+    link: 'https://www.linkedin.com/in/danielarbesu/'
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/daniArbesu'
+  }
+];
+
+export const projectLinks = [
+  {
+    name: '',
+    image: '',
+    link: ''
+  }
+];
+
+export const resumeLink =
+  'https://drive.google.com/file/d/1P2q3GneVdGBKBs-7JXJj9D89DLt9xK-4/view?usp=sharing';
