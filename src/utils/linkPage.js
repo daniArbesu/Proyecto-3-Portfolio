98befc69429854922af8ec64fd4cb7442ecc5382
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-05-08 18:15:03 +0200

    fix: bug when selecting link from navbar

--- src/utils/linkPage.js
+++ src/utils/linkPage.js
@@ -1,9 +1,22 @@
+import icons from './icons';
+
 const linkPage = (id, page) => {
   const links = document.querySelectorAll(id);
+  const hamburgerpath = document.querySelector('#mobile-menu__icon-path');
+
   links.forEach((link) =>
     link.addEventListener('click', () => {
       page();
       document.querySelector('#mobile-menu__nav').classList.toggle('showmobile-menu__nav');
+
+      const isMenuClose =
+        document.querySelector('#mobile-menu__nav').getAttribute('class') ===
+        'mobile-menu__nav showmobilelinks';
+      if (isMenuClose) {
+        hamburgerpath.setAttribute('d', icons.hamburgerOpen);
+      } else {
+        hamburgerpath.setAttribute('d', icons.hamburgerClose);
+      }
     })
   );
 };
